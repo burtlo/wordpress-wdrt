f47e43b5da8a92ada3346dbe4f3900c0d67c011c
Franklin Webber <franklin.webber@gmail.com>
2016-02-17 00:13:20 -0600

    Remove the address specification
    Testing a theory that the address is causing the user account to be created in the wrong account than the one that wordpress is wanting to have connect.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,6 @@
 
 node.default['wordpress']['db']['user'] = 'wp_user'
 node.set['wordpress']['db']['pass'] = 'something_more_secure'
-node.default['wordpress']['db']['host']  = "127.0.0.1"
+# node.default['wordpress']['db']['host']  = "127.0.0.1"
 
 include_recipe 'wordpress::default'
