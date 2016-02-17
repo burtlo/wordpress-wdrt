44e54d58031df5893293d4ec998790d03560f2bc
Franklin Webber <franklin.webber@gmail.com>
2016-02-16 22:37:16 -0600

    Replace the metadata.json with ruby metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+ame 'wordpress-wdrt'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures wordpress-wdrt'
+long_description 'Installs/Configures wordpress-wdrt'
+version '0.1.0'
+
+depends 'wordpress'
