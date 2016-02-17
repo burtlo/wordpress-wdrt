19a925a8152977fc5b48da529d47f159c0ebfe4b
Franklin Webber <franklin.webber@gmail.com>
2016-02-17 01:13:32 -0600

    Update to fix the missing connection info

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,13 @@ include_recipe 'wordpress::default'
 
 db = node['wordpress']['db']
 
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :socket   => "/var/run/mysql-#{db['instance_name']}/mysqld.sock",
+  :password => db['root_password']
+}
+
 mysql_database_user db['user'] do
   connection    mysql_connection_info
   database_name db['name']
