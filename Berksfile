cb3fa7799c9997b021a05086f8bd41fa5a47c12e
Franklin Webber <franklin.webber@gmail.com>
2016-02-17 01:30:43 -0600

    Update with new workaround
    To get around the database user not having a password I forked the wordpress cookbook and now I am using that fork which hopefully creates the user with the password correctly.

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://supermarket.chef.io"
 
 metadata
+
+cookbook 'wordpress', git: 'git@github.com:burtlo/wordpress-cookbook.git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,20 +9,3 @@ node.default['wordpress']['db']['user'] = 'wp_user'
 node.set['wordpress']['db']['pass'] = 'something_more_secure'
 
 include_recipe 'wordpress::default'
-
-db = node['wordpress']['db']
-
-mysql_connection_info = {
-  :host     => 'localhost',
-  :username => 'root',
-  :socket   => "/var/run/mysql-#{db['instance_name']}/mysqld.sock",
-  :password => db['root_password']
-}
-
-mysql_database_user db['user'] do
-  connection    mysql_connection_info
-  database_name db['name']
-  password      db['password']
-  privileges    [:all]
-  action        :grant
-end
