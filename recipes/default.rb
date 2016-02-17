09545e8e98d610e38d0304c1772df29c663ba179
Franklin Webber <franklin.webber@gmail.com>
2016-02-16 23:53:22 -0600

    Add attributes for user and password:

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 
+node.default['wordpress']['db']['user'] = 'wp_user'
+node.set['wordpress']['db']['pass'] = 'something_more_secure'
 node.default['wordpress']['db']['host']  = "127.0.0.1"
 
 include_recipe 'wordpress::default'
